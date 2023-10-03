b2de29ff147415b5624ccfb3d11247fc5cb2f5f4
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-10-03 18:34:51 +0530

    update

--- data-gen.py
+++ data-gen.py
@@ -13,16 +13,79 @@ def generate_roll_numbers():
         roll_numbers.append(roll_number)
     return roll_numbers
 
+def generate_names():
+    names = []
+    for name in range(1,100):
+        newName = fake.name()
+        names.append(newName)
+    return names
+
+def generate_mapping(roll_numbers, student_names):
+    mappings = {}
+    for i in range(len(roll_numbers)):
+        roll_number = roll_numbers[i]
+        student_name = student_names[i]
+        mappings[roll_number] = student_name
+    return mappings
 
 roll_numbers = generate_roll_numbers()
+student_names = generate_names()
 mess_price_ranges = { '15 Days' : '1500',  '20 Days': '2000', '25 Days': '2500' }
+course_types = ['BTech', 'Mtech', 'PhD']
+room_types = ['Single', 'Double', 'Married']
+floors = ['Ground', 'First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh', 'Eighth', 'Nineth', 'Tenth']
+roll_no_name_mapping = generate_mapping(roll_numbers, student_names)
 
 def generate_hostel_data(num_records):
     with open('./Data/hostel_data.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["Sr. No", "Room Type", "Floor", "Room No", "Student Name", "Roll No", "Course Type", "Year", "Email ID", "Fees", "Security Amt", "Bank ID", "Contact", "Remarks", "From", "To", "Sharing", "Name", "Roll No", "Course", "Year", "Email ID"])  # header
+        writer.writerow(["Sr. No", 
+                         "Room Type",
+                         "Floor",
+                         "Room No", 
+                         "Student Name"
+                         "Roll No",
+                         "Course Type",
+                         "Year",
+                         "Email ID",
+                         "Fees",
+                         "Security Amt",
+                         "Bank ID", 
+                         "Contact", 
+                         "Remarks", 
+                         "From", 
+                         "To", 
+                         "Sharing",
+                         "Name", 
+                         "Roll No", 
+                         "Course", 
+                         "Year", 
+                         "Email ID"])  
         for i in range(num_records):
-            writer.writerow([i+1, fake.random.choice(['Single', 'Double', 'Married']), fake.word(), f"A-{fake.random_number(digits=3)}", fake.name(), random.choice(roll_numbers), fake.word(), fake.random_number(digits=1), fake.email(), fake.random_number(digits=5), fake.random_number(digits=5), fake.random_number(digits=5), fake.phone_number(), fake.text(), fake.date(), fake.date(), fake.name(), random.choice(roll_numbers), fake.word(), fake.random_number(digits=1), fake.email()])
+            roll_number = random.choice(roll_numbers)
+            name = roll_no_name_mapping[roll_number]
+            writer.writerow([i+1,
+                             fake.random.choice(room_types),
+                             fake.random.choice(floors),
+                             f"A-{fake.random_number(digits=3)}",
+                             name,
+                             roll_number,
+                             fake.random.choice(course_types),
+                             fake.random_number(digits=1),
+                             fake.email(),
+                             fake.random_number(digits=5),
+                             fake.random_number(digits=5),
+                             fake.random_number(digits=5),
+                             fake.phone_number(),
+                             fake.text(),
+                             fake.date(),
+                             fake.date(),
+                             fake.name(),
+                             random.choice(roll_numbers),
+                             fake.word(),
+                             fake.random_number(digits=1),
+                             fake.email()
+                             ])
 
 def generate_mess_1_data(num_records):
     with open('./Data/mess_1_data.csv', mode='w', newline='') as file:
