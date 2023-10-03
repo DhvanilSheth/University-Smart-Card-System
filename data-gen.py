cff6277c94365f7a3b31c76db477f3ac5970eadb
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-10-03 16:07:09 +0530

    update

--- data-gen.py
+++ data-gen.py
@@ -13,22 +13,30 @@ def generate_roll_numbers():
         roll_numbers.append(roll_number)
     return roll_numbers
 
+
+roll_numbers = generate_roll_numbers()
+mess_price_ranges = { '15 Days' : '1500',  '20 Days': '2000', '25 Days': '2500' }
+
 def generate_hostel_data(num_records):
-    roll_numbers = generate_roll_numbers()  # Generate roll numbers
     with open('./Data/hostel_data.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Sr. No", "Room Type", "Floor", "Room No", "Student Name", "Roll No", "Course Type", "Year", "Email ID", "Fees", "Security Amt", "Bank ID", "Contact", "Remarks", "From", "To", "Sharing", "Name", "Roll No", "Course", "Year", "Email ID"])  # header
         for i in range(num_records):
             writer.writerow([i+1, fake.random.choice(['Single', 'Double', 'Married']), fake.word(), f"A-{fake.random_number(digits=3)}", fake.name(), random.choice(roll_numbers), fake.word(), fake.random_number(digits=1), fake.email(), fake.random_number(digits=5), fake.random_number(digits=5), fake.random_number(digits=5), fake.phone_number(), fake.text(), fake.date(), fake.date(), fake.name(), random.choice(roll_numbers), fake.word(), fake.random_number(digits=1), fake.email()])
 
-def generate_mess_data(num_records):
-    roll_numbers = generate_roll_numbers()  # Generate roll numbers
-    with open('./Data/mess_data.csv', mode='w', newline='') as file:
+def generate_mess_1_data(num_records):
+    with open('./Data/mess_1_data.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
-        # header
         writer.writerow(["Sale ID", "Sale Date", "Coupon Type", "Name", "Phone Number", "Roll NO", "Paytm", "Cash", "Total Amount"])
         for i in range(num_records):
-            writer.writerow([i + 1, fake.date_time(), fake.random.choice(['7 days', '30 days']), fake.name(), fake.phone_number(), random.choice(roll_numbers), fake.random_number(digits=6), fake.random_number(digits=3), fake.random_number(digits=4)])
+            choice_of_payment = random.random()
+            type = random.choice(list(mess_price_ranges.keys()))
+            cost = mess_price_ranges[type]
+            mobile_number = fake.random_number(digits=10)
+            if choice_of_payment == 1:
+                writer.writerow([i + 1, fake.date_time(), type, fake.name(), mobile_number, random.choice(roll_numbers), cost, 0, cost])
+            else:
+                writer.writerow([i + 1, fake.date_time(), type, fake.name(), mobile_number, random.choice(roll_numbers), 0, cost, cost])
 
 
 def generate_bms_hk_visitor_data(num_records):
@@ -48,7 +56,6 @@ def generate_package_collection_data(num_records):
 
 
 def generate_home_leave_data(num_records):
-    roll_numbers = generate_roll_numbers()
     with open('./Data/home_leave_data.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Date", "Name", "Contact", "Out Time", "Roll No", "Room No", "To Address", "Return Time", "Return Date", "Signature", "Security Signature"])
@@ -57,7 +64,6 @@ def generate_home_leave_data(num_records):
 
 
 def generate_medicine_data(num_records):
-    roll_numbers = generate_roll_numbers()
     with open('./Data/medicine_data.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Date", "Name", "Contact", "Medicine Name", "Quantity", "Room No", "Time", "Roll No", "Signature", "Purpose", "Security Signature"])
@@ -66,7 +72,6 @@ def generate_medicine_data(num_records):
 
 
 def generate_sports_data(num_records):
-    roll_numbers = generate_roll_numbers()
     with open('./Data/sports_data.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Date", "Name", "Contact", "Sport", "Equipment", "Room No", "Time", "Roll No", "Signature", "Security Signature"])
@@ -74,23 +79,9 @@ def generate_sports_data(num_records):
             writer.writerow([fake.date(), fake.name(), fake.phone_number(), fake.word(), fake.word(), f"A-{fake.random_number(digits=3)}", fake.time(), fake.random_number(digits=6), fake.name(), fake.name()])
 
 generate_hostel_data(100)
-generate_mess_data(100)
+generate_mess_1_data(100)
 generate_bms_hk_visitor_data(100)
 generate_package_collection_data(100)
 generate_home_leave_data(100)
 generate_medicine_data(100)
 generate_sports_data(100)
-
-
-
-# def generate_roll_numbers():
-#     roll_numbers = []
-#     for roll in range(1, 100):
-#         year_of_admission = random.randint(2018, 2024)
-#         roll_number_part = f"{roll:03d}"
-#         roll_number = f"{year_of_admission}{roll_number_part}"
-#         roll_numbers.append(roll_number)
-#     return roll_numbers
-
-# roll_numbers = generate_roll_numbers()
-# print(roll_numbers)
