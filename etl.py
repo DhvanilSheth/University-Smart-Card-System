cdf10de3713c7781421cf59ec4e9cce33ca437d2
DhvanilSheth <dhvanil21040@iiitd.ac.in>
2023-10-22 20:39:11 +0530

    Final ETL Changes + new CSVs

--- etl.py
+++ etl.py
@@ -4,35 +4,35 @@ def alter_primary_keys(host, user, password):
     connection = mysql.connector.connect(host=host, user=user, password=password)
     cursor = connection.cursor()
 
-    # List of commands to alter primary keys
-    commands = [
-        # SportsDB
-        ("USE SportsDB;",),
-        ("ALTER TABLE pool ADD PRIMARY KEY(Roll_No, Sr_No);",),
-        ("ALTER TABLE pool_non_membership ADD PRIMARY KEY(Roll_No, In_Time, Date);",),
-        ("ALTER TABLE medicine_sports ADD PRIMARY KEY(Contact_No, Time, Date);",),
-        ("ALTER TABLE gym ADD PRIMARY KEY(Roll_No, In_Time, Date);",),
-        ("ALTER TABLE equipment_requests ADD PRIMARY KEY(Roll_No, In_Time, Date);",),
-        ("ALTER TABLE equipment_loss ADD PRIMARY KEY(Roll_No, Time, Date);",),
+    dbs_tables_keys = {
+        'SportsDB': {
+            'pool': ['Roll_No', 'Sr_No'],
+            'pool_non_membership': ['Roll_No', 'In_Time', 'Date'],
+            'medicine_sports': ['Contact_No', 'Time', 'Date'],
+            'gym': ['Roll_No', 'In_Time', 'Date'],
+            'equipment_requests': ['Roll_No', 'In_Time', 'Date'],
+            'equipment_loss': ['Roll_No', 'Time', 'Date']
+        },
+        'MessDB': {
+            'mess_1_data': ['Roll_No', 'Sr_No'],
+            'mess_2_data': ['Roll_No', 'Sale_ID']
+        },
+        'HostelDB': {
+            'home_leave_data': ['Roll_No', 'Out_Time', 'Date'],
+            'hostel_data': ['Roll_No', 'Sr_No'],
+            'medicine_data': ['Contact', 'Time', 'Date'],
+            'package_collection_data': ['Pick_Up_Student_Number', 'Driver_Contact', 'Sr_No']
+        }
+    }
 
-        # MessDB
-        ("USE MessDB;",),
-        ("ALTER TABLE mess_1_data ADD PRIMARY KEY(Roll_No, Sr_No);",),
-        ("ALTER TABLE mess_2_data ADD PRIMARY KEY(Roll_No, Sale_ID);",),
-
-        # HostelDB
-        ("USE HostelDB;",),
-        ("ALTER TABLE home_leave_data ADD PRIMARY KEY(Roll_No, Out_Time, Date);",),
-        ("ALTER TABLE hostel_data ADD PRIMARY KEY(Roll_No, Sr_No);",),
-        ("ALTER TABLE medicine_data ADD PRIMARY KEY(Contact, Time, Date);",),
-        ("ALTER TABLE package_collection_data ADD PRIMARY KEY(Pick_Up_Student_Number, Driver_Contact, Sr_No);",)
-    ]
-
-    for command in commands:
-        try:
-            cursor.execute(*command)
-        except mysql.connector.Error as err:
-            print(f"Error executing {command}: {err}")
+    for db, tables in dbs_tables_keys.items():
+        cursor.execute(f"USE {db};")
+        for table, keys in tables.items():
+            combined_keys = ", ".join(keys)
+            try:
+                cursor.execute(f"ALTER TABLE {table} ADD PRIMARY KEY ({combined_keys});")
+            except mysql.connector.Error as err:
+                print(f"Error executing command on table {table}: {err}")
 
     cursor.close()
     connection.close()
