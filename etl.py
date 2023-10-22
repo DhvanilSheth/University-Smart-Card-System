74e1d063aeffc195de9d0f0f9c13f438f996cb3e
DhvanilSheth <dhvanil21040@iiitd.ac.in>
2023-10-22 16:37:58 +0530

    ETL CHANGES BOOM BAAM

--- etl.py
+++ etl.py
@@ -8,24 +8,24 @@ def alter_primary_keys(host, user, password):
     commands = [
         # SportsDB
         ("USE SportsDB;",),
-        ("ALTER TABLE pool DROP PRIMARY KEY, ADD PRIMARY KEY(Roll_No, Sr_No);",),
-        ("ALTER TABLE pool_non_membership DROP PRIMARY KEY, ADD PRIMARY KEY(Roll_No, In_Time, Date);",),
-        ("ALTER TABLE medicine_sport DROP PRIMARY KEY, ADD PRIMARY KEY(Contact_No, Time, Date);",),
-        ("ALTER TABLE gym DROP PRIMARY KEY, ADD PRIMARY KEY(Roll_No, In_Time, Date);",),
-        ("ALTER TABLE equipment_requests DROP PRIMARY KEY, ADD PRIMARY KEY(Roll_No, In_Time, Date);",),
-        ("ALTER TABLE equipment_loss DROP PRIMARY KEY, ADD PRIMARY KEY(Roll_No, Time, Date);",),
+        ("ALTER TABLE pool ADD PRIMARY KEY(Roll_No, Sr_No);",),
+        ("ALTER TABLE pool_non_membership ADD PRIMARY KEY(Roll_No, In_Time, Date);",),
+        ("ALTER TABLE medicine_sports ADD PRIMARY KEY(Contact_No, Time, Date);",),
+        ("ALTER TABLE gym ADD PRIMARY KEY(Roll_No, In_Time, Date);",),
+        ("ALTER TABLE equipment_requests ADD PRIMARY KEY(Roll_No, In_Time, Date);",),
+        ("ALTER TABLE equipment_loss ADD PRIMARY KEY(Roll_No, Time, Date);",),
 
         # MessDB
         ("USE MessDB;",),
-        ("ALTER TABLE mess1 DROP PRIMARY KEY, ADD PRIMARY KEY(Roll_No, Sr_No);",),
-        ("ALTER TABLE mess2 DROP PRIMARY KEY, ADD PRIMARY KEY(Roll_No, Sale_ID);",),
+        ("ALTER TABLE mess_1_data ADD PRIMARY KEY(Roll_No, Sr_No);",),
+        ("ALTER TABLE mess_2_data ADD PRIMARY KEY(Roll_No, Sale_ID);",),
 
         # HostelDB
         ("USE HostelDB;",),
-        ("ALTER TABLE home_leave_data DROP PRIMARY KEY, ADD PRIMARY KEY(Roll_No, Out_Time, Date);",),
-        ("ALTER TABLE hostel_data DROP PRIMARY KEY, ADD PRIMARY KEY(Roll_No, Sr_No);",),
-        ("ALTER TABLE medicine_data DROP PRIMARY KEY, ADD PRIMARY KEY(Contact, Time, Date);",),
-        ("ALTER TABLE package_collection DROP PRIMARY KEY, ADD PRIMARY KEY(Student_Contact, Driver_Contact, Sr_No);",)
+        ("ALTER TABLE home_leave_data ADD PRIMARY KEY(Roll_No, Out_Time, Date);",),
+        ("ALTER TABLE hostel_data ADD PRIMARY KEY(Roll_No, Sr_No);",),
+        ("ALTER TABLE medicine_data ADD PRIMARY KEY(Contact, Time, Date);",),
+        ("ALTER TABLE package_collection_data ADD PRIMARY KEY(Pick_Up_Student_Number, Driver_Contact, Sr_No);",)
     ]
 
     for command in commands:
