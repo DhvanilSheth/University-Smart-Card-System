90b1ad1878156ab32cb7593686eb076edc0fd327
aajaydev <aajay.devaraj@gmail.com>
2023-10-28 18:48:51 +0530

    siu

--- dynamic-data.py
+++ dynamic-data.py
@@ -2,9 +2,14 @@ import mysql.connector
 import pandas as pd
 import json
 
+def clean_df_columns(df):
+    """Sanitize DataFrame column names by replacing spaces with underscores."""
+    df.columns = [col.replace(" ", "_") for col in df.columns]
+    return df
+
 # Read the configuration file
 with open('data_sources_config.json', 'r') as config_file:
-    tables_data = json.load(config_file)
+    config_data = json.load(config_file)
 
 # Connect to the MySQL server
 connection = mysql.connector.connect(
@@ -12,34 +17,40 @@ connection = mysql.connector.connect(
     user="root",
     password="akis@123",
 )
-
 cursor = connection.cursor()
 
-# Create the databases dynamically from the configuration file
-for table_info in tables_data:
-    table_name = table_info['table']
-    csv_path = table_info['path']
-    db_name = table_info['db_name']
-    headers = table_info['headers']
-    
-    # Create the database if it doesn't exist
-    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+# For each database and its tables defined in the config file
+for db_name, table_infos in config_data.items():
+    cursor.execute(f"USE {db_name}")
 
-    # Create table if it doesn't exist
-    columns_definitions = ', '.join([f"{header['Name'].replace(' ', '_')} {header['Type']}" for header in headers])
-    create_table_query = f"CREATE TABLE IF NOT EXISTS {db_name}.{table_name} ({columns_definitions})"
-    cursor.execute(create_table_query)
+    for table_info in table_infos:
+        # Extract table details from the configuration
+        table_name = table_info['table']
+        csv_path = table_info['path']
+        headers = table_info['headers']
 
-    # Read CSV and insert data
-    df = pd.read_csv(csv_path)
-    columns = df.columns.tolist()
-    values = df.values.tolist()
+        # Construct the CREATE TABLE query
+        columns_definitions = ', '.join([f"{header['Name'].replace(' ', '_')} {header['Type']}" for header in headers])
+        primary_keys = ', '.join([pk.replace(' ', '_') for pk in table_info['primary_key']])
+        create_table_query = f"""
+            CREATE TABLE IF NOT EXISTS {table_name} (
+                {columns_definitions},
+                PRIMARY KEY ({primary_keys})
+            )
+        """
+        cursor.execute(create_table_query)
 
-    for value_set in values:
-        insert_query = f"INSERT IGNORE INTO {db_name}.{table_name} ({', '.join(columns)}) VALUES ({', '.join(['%s'] * len(columns))})"
-        cursor.execute(insert_query, tuple(value_set))
-    
-    connection.commit()
+        # Read CSV and insert data
+        df = pd.read_csv(csv_path)
+        df = clean_df_columns(df)
+        columns = df.columns.tolist()
+        values = df.values.tolist()
+
+        for value_set in values:
+            insert_query = f"INSERT IGNORE INTO {table_name} ({', '.join(columns)}) VALUES ({', '.join(['%s'] * len(columns))})"
+            cursor.execute(insert_query, tuple(value_set))
+
+        connection.commit()
 
 cursor.close()
 connection.close()
