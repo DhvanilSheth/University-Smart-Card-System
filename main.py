c67e4955babe633ce28db195d38471d935238771
vish <vishweshvhavle@gmail.com>
2023-11-22 21:57:38 +0530

    Fixed file name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from extract import *
-from transform2 import *
-from load2 import *
+from transform import *
+from load import *
 
 import mysql.connector
 import json
@@ -19,8 +19,8 @@ def main():
 		data_config = json.load(file)
 
 	sports_dfs, hostel_dfs, mess_dfs, admin_dfs, access_dfs = extract(SERVER_IP, SERVER_USER, SERVER_PASSWORD)
-	integrated_df = transform(sports_dfs, hostel_dfs, mess_dfs, admin_dfs, access_dfs)
-	load(integrated_df, SERVER_IP, SERVER_USER, SERVER_PASSWORD)
+	integrated_dfs = transform(sports_dfs, hostel_dfs, mess_dfs, admin_dfs, access_dfs)
+	load(integrated_dfs, SERVER_IP, SERVER_USER, SERVER_PASSWORD)
 
 if __name__ == "__main__":
     main()
