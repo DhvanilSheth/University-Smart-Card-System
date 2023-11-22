d10bbce100914e008617e4a6e5a00ddde87c648f
vish <vishweshvhavle@gmail.com>
2023-11-22 18:27:58 +0530

    Added main for extraction

---
+++ main.py
@@ -0,0 +1,23 @@
+from extract import *
+from transform import *
+from load import *
+
+import mysql.connector
+import json
+import pandas as pd
+
+SERVER_IP = '192.168.32.187'
+SERVER_USER = 'root'
+SERVER_PASSWORD = 'vhavle'
+
+def main():
+	with open('data_sources_config.json', 'r') as file:
+		data_config = json.load(file)
+
+	sports_data, hostel_data, mess_data = extract(SERVER_IP, SERVER_USER, SERVER_PASSWORD)
+	integrated_data = transform(sports_data, hostel_data, mess_data)
+	print(integrated_data.head())
+	# load(integrated_data, SERVER_IP, SERVER_USER, SERVER_PASSWORD)
+
+if __name__ == "__main__":
+    main()
